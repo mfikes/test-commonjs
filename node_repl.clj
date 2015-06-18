f003910a460f258e9849dfeab5cb506f5ec59366
Mike Fikes <mike@fikesfarm.com>
2015-06-17 21:53:46 -0400

    Revert "Update for CLJS-1313 testing"
    This reverts commit 784d7d52293a876a2a326dec18549293378edfc0.

--- node_repl.clj
+++ node_repl.clj
@@ -1,11 +1,21 @@
 (require 'cljs.repl)
+(require 'cljs.build.api)
 (require 'cljs.repl.node)
 
+(def foreign-libs [{:file "libs/greeting.js"
+       :provides ["greeting"]
+       :module-type :commonjs}
+      {:file "libs/german.js"
+       :provides ["german"]
+       :module-type :commonjs}])
+
+(cljs.build.api/build "src"
+  {:main 'foo.bar
+   :output-to "out/main.js"
+   :verbose true
+   :foreign-libs foreign-libs})
+
 (cljs.repl/repl (cljs.repl.node/repl-env)
+  :watch "src"
   :output-dir "out"
-  :foreign-libs [{:file "libs/greeting.js"
-                  :provides ["greeting"]
-                  :module-type :commonjs}
-                 {:file "libs/german.js"
-                  :provides ["german"]
-                  :module-type :commonjs}])
+  :foreign-libs foreign-libs)
